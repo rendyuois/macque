6941ea1e7e8a9181a9cab7512a7613d976b675ba
rendyuois <rendyuois@icloud.com>
2023-10-15 00:26:58 +0700

    #ubah src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn do_hello() {
+    println!("kostumisasi git project!")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
